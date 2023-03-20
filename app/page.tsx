5616809ce3d2df1ff6df3f9f758d8107516c35b1
Anil Pak <anil.pak@performio.co>
2023-03-20 20:11:36 +1100

    Layout added.

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,39 @@
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+'use client'
 
-const inter = Inter({ subsets: ['latin'] })
+import Link from "next/link";
+import { Flex, Text, Grid, GridItem } from "@chakra-ui/react";
+import Greetings from "./components/greetings";
 
 export default function Home() {
   return (
-    <main>
-      Hey Main Page!
-    </main>
+    <Grid 
+      w={'60%'} 
+      h='100vh'
+      margin="auto"
+      templateRows={'4 1fr'}
+    >
+      <GridItem margin={'auto'} rowSpan={1}>
+        <Text fontSize='7xl'>
+          {Greetings()}, this is Anıl 👋🏿
+        </Text>
+      </GridItem>
+      <GridItem margin={'auto'} rowSpan={1}>
+        <Flex justifyContent={'space-between'} gap={'5'}>
+          <Link href="/about-me"><Text fontSize='xl'>About me</Text></Link>
+          <Link href="/blog"><Text fontSize='xl'>Blog</Text></Link>
+          <Link href="/reading-list"><Text fontSize='xl'>Reading List</Text></Link>
+          <Link href="/contact"><Text fontSize='xl'>Contact</Text></Link>
+        </Flex>
+      </GridItem>
+      <GridItem margin={'auto'} rowSpan={2}>
+          <Text fontSize='4xl'>
+            Self-taught software engineer in Melbourne, Australia. <br />
+            I love JavaScript and I am interested in inclusive design and web accessibility.
+          </Text>
+      </GridItem>
+      <GridItem margin={'auto'} rowSpan={1}>
+        <Text fontSize='xs'>© 2023 All rights reserved.</Text> 
+      </GridItem>
+    </Grid>
   )
 }
